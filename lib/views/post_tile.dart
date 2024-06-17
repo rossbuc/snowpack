8800a61dbfe3d1cf2b6ab28342928b6e36e41f36
Ross Buchan <rossbuchan1@googlemail.com>
2024-06-17 15:21:08 +0100

    Update list tile styling

--- lib/views/post_tile.dart
+++ lib/views/post_tile.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:snowpack/models/post.dart';
 import 'package:snowpack/models/user.dart';
 
@@ -9,7 +11,7 @@ class PostTile extends StatelessWidget {
   const PostTile({super.key, required this.post, required this.user});
 
   String getFormattedDate() {
-    return "${post.dateTime.day}/${post.dateTime.month}/${post.dateTime.year}";
+    return "${post.dateTime.day}/${post.dateTime.month}/${post.dateTime.year} at ${post.dateTime.hour}:${post.dateTime.minute}";
   }
 
   @override
@@ -26,12 +28,34 @@ class PostTile extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                post.userId.toString(),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    CupertinoIcons.person_alt_circle,
+                    size: 32,
+                  ),
+                  const SizedBox(width: 8),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        user.username,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        getFormattedDate(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
               const SizedBox(height: 16),
               Text(
