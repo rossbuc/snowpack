a955115445dc0d6e025a2325e98a0bdd96be0d75
Ross Buchan <rossbuchan1@googlemail.com>
2024-07-18 16:39:35 +0100

    Encorporate state into feed to conditionally render SafeArea

--- lib/views/feed.dart
+++ lib/views/feed.dart
@@ -6,11 +6,35 @@ import 'package:snowpack/models/aspect.dart';
 import 'package:snowpack/services/post_service.dart';
 import 'package:snowpack/views/post_list.dart';
 
-class Feed extends ConsumerWidget {
+class Feed extends ConsumerStatefulWidget {
   const Feed({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _FeedState createState() => _FeedState();
+}
+
+class _FeedState extends ConsumerState<Feed> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isAppBarVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        _isAppBarVisible = _scrollController.position.pixels < 100;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final postService = ref.read(postServiceProvider.notifier);
 
@@ -20,77 +44,105 @@ class Feed extends ConsumerWidget {
     }
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: true,
-            // snap: true,
-            // expandedHeight: 120,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [colorScheme.primary, colorScheme.secondary],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            elevation: 4.0,
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 4.0, bottom: 4),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(16),
-                  splashColor: colorScheme.secondary.withOpacity(0.5),
-                  onTap: _onLogoTap,
-                  child: SizedBox(
-                    width: 50, // Adjust the width as needed
-                    height: 50, // Adjust the height as needed
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Image.asset(
-                        "assets/images/SnowPack Logo Symbol.png",
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: IconButton(
-                  icon: const Icon(CupertinoIcons.gear_alt),
-                  onPressed: () {
-                    settingsPressed();
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: PopupMenuButton<String>(
-                  icon: const Icon(CupertinoIcons.line_horizontal_3),
-                  onSelected: (String result) {
-                    postService.sortPosts(result);
-                  },
-                  itemBuilder: (BuildContext context) =>
-                      <PopupMenuEntry<String>>[
-                    const PopupMenuItem<String>(
-                      value: 'time',
-                      child: Text('Sort by Time'),
-                    ),
-                    const PopupMenuItem<String>(
-                      value: 'location',
-                      child: Text('Sort by Location'),
-                    ),
-                  ],
-                ),
-              ),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (scrollNotification) {
+          if (scrollNotification is ScrollUpdateNotification) {
+            setState(() {
+              _isAppBarVisible = _scrollController.position.pixels < 100;
+            });
+          }
+          return true;
+        },
+        child: SafeArea(
+          top:
+              _isAppBarVisible, // Conditionally render SafeArea based on AppBar visibility
+          child: CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              _buildSliverAppBar(
+                  context, colorScheme, settingsPressed, postService),
+              const PostList(),
             ],
           ),
-          const PostList(),
+        ),
+      ),
+    );
+  }
+
+  SliverAppBar _buildSliverAppBar(BuildContext context, ColorScheme colorScheme,
+      void Function() settingsPressed, PostService postService) {
+    return SliverAppBar(
+      floating: true,
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [colorScheme.primary, colorScheme.secondary],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      ),
+      elevation: 4.0,
+      leading: _buildLogoButton(context, colorScheme),
+      actions: [
+        _buildSettingsButton(settingsPressed),
+        _buildSortButton(postService),
+      ],
+    );
+  }
+
+  Padding _buildLogoButton(BuildContext context, ColorScheme colorScheme) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 4.0, bottom: 4),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(16),
+          splashColor: colorScheme.secondary.withOpacity(0.5),
+          onTap: _onLogoTap,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                "assets/images/SnowPack Logo Symbol.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _buildSettingsButton(void Function() settingsPressed) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: IconButton(
+        icon: const Icon(CupertinoIcons.gear_alt),
+        onPressed: settingsPressed,
+      ),
+    );
+  }
+
+  Padding _buildSortButton(PostService postService) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: PopupMenuButton<String>(
+        icon: const Icon(CupertinoIcons.line_horizontal_3),
+        onSelected: (String result) {
+          postService.sortPosts(result);
+        },
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          const PopupMenuItem<String>(
+            value: 'time',
+            child: Text('Sort by Time'),
+          ),
+          const PopupMenuItem<String>(
+            value: 'location',
+            child: Text('Sort by Location'),
+          ),
         ],
       ),
     );
@@ -105,7 +157,6 @@ class Feed extends ConsumerWidget {
     final initialAspectValue = postService.currentAspectFilter;
     final initialTemperatureValue = postService.currentTemperatureFilter ?? 0;
 
-    // Aspect Filter Dropdown
     const aspects = Aspect.values;
 
     showDialog(
@@ -116,61 +167,11 @@ class Feed extends ConsumerWidget {
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              // Elevation Filter
-              DropdownButton<int>(
-                value: initialElevationValue,
-                onChanged: (value) {
-                  if (value != null) {
-                    postService.setElevationFilter(value);
-                    print("Selected Elevation: $value ft");
-                  }
-                },
-                items: List.generate(
-                  110, // Adjust based on the range of elevation you want to display
-                  (index) => DropdownMenuItem<int>(
-                    value: index * 100,
-                    child: Text('${index * 100} ft'),
-                  ),
-                ),
-              ),
+              _buildElevationDropdown(postService, initialElevationValue),
               SizedBox(height: 20),
-              // Aspect Filter
-              DropdownButton<Aspect>(
-                hint: Text('Select Aspect'),
-                value: initialAspectValue,
-                onChanged: (value) {
-                  if (value != null) {
-                    postService.setAspectFilter(value);
-                    print(
-                        "Selected Aspect: ${value.toString().split('.').last}");
-                  }
-                },
-                items: aspects.map((Aspect aspect) {
-                  return DropdownMenuItem<Aspect>(
-                    value: aspect,
-                    child: Text(aspect.toString().split('.').last),
-                  );
-                }).toList(),
-              ),
+              _buildAspectDropdown(postService, aspects, initialAspectValue),
               SizedBox(height: 20),
-              // Temperature Filter
-              DropdownButton<int>(
-                hint: Text("Select Temperature Range"),
-                value: initialTemperatureValue,
-                onChanged: (value) {
-                  if (value != null) {
-                    postService.setTemperatureFilter(value);
-                    print("Selected Temperature: $value degrees");
-                  }
-                },
-                items: List.generate(
-                  10, // Adjust based on the range of temperature you want to display
-                  (index) => DropdownMenuItem<int>(
-                    value: (index - 5) * 5,
-                    child: Text('${(index - 5) * 5} degrees'),
-                  ),
-                ),
-              )
+              _buildTemperatureDropdown(postService, initialTemperatureValue),
             ],
           ),
           actions: <Widget>[
@@ -181,14 +182,76 @@ class Feed extends ConsumerWidget {
               child: const Text('Close'),
             ),
             TextButton(
-                onPressed: () {
-                  postService.clearFilters();
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Reset"))
+              onPressed: () {
+                postService.clearFilters();
+                Navigator.of(context).pop();
+              },
+              child: const Text("Reset"),
+            ),
           ],
         );
       },
+    );
+  }
+
+  DropdownButton<int> _buildElevationDropdown(
+      PostService postService, int initialElevationValue) {
+    return DropdownButton<int>(
+      value: initialElevationValue,
+      onChanged: (value) {
+        if (value != null) {
+          postService.setElevationFilter(value);
+          print("Selected Elevation: $value ft");
+        }
+      },
+      items: List.generate(
+        110,
+        (index) => DropdownMenuItem<int>(
+          value: index * 100,
+          child: Text('${index * 100} ft'),
+        ),
+      ),
+    );
+  }
+
+  DropdownButton<Aspect> _buildAspectDropdown(PostService postService,
+      List<Aspect> aspects, Aspect? initialAspectValue) {
+    return DropdownButton<Aspect>(
+      hint: Text('Select Aspect'),
+      value: initialAspectValue,
+      onChanged: (value) {
+        if (value != null) {
+          postService.setAspectFilter(value);
+          print("Selected Aspect: ${value.toString().split('.').last}");
+        }
+      },
+      items: aspects.map((Aspect aspect) {
+        return DropdownMenuItem<Aspect>(
+          value: aspect,
+          child: Text(aspect.toString().split('.').last),
+        );
+      }).toList(),
+    );
+  }
+
+  DropdownButton<int> _buildTemperatureDropdown(
+      PostService postService, int initialTemperatureValue) {
+    return DropdownButton<int>(
+      hint: Text("Select Temperature Range"),
+      value: initialTemperatureValue,
+      onChanged: (value) {
+        if (value != null) {
+          postService.setTemperatureFilter(value);
+          print("Selected Temperature: $value degrees");
+        }
+      },
+      items: List.generate(
+        10,
+        (index) => DropdownMenuItem<int>(
+          value: (index - 5) * 5,
+          child: Text('${(index - 5) * 5} degrees'),
+        ),
+      ),
     );
   }
 }
