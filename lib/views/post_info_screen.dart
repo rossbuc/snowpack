dd0d03966e904c436e528a126f7f5f4656f401e0
Ross Buchan <rossbuchan1@googlemail.com>
2024-08-10 19:43:56 +0100

    Add edit post page

---
+++ lib/views/post_info_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:snowpack/models/post.dart';
+import 'package:snowpack/models/user.dart';
+
+class PostInfoScreen extends StatelessWidget {
+  final Post post;
+  final User user;
+
+  const PostInfoScreen({super.key, required this.post, required this.user});
+
+  String getFormattedDate() {
+    return "${post.dateTime.day}/${post.dateTime.month}/${post.dateTime.year} at ${post.dateTime.hour}:${post.dateTime.minute}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(post.title),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              PostEditView(post: post, user: user)));
+                },
+                child: const Icon(CupertinoIcons.ellipsis)),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(
+                  CupertinoIcons.person_alt_circle,
+                  size: 32,
+                ),
+                const SizedBox(width: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      user.username,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      getFormattedDate(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Text(
+              post.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Aspect",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(post.aspect.toString().split('.').last),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Elevation",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("${post.elevation.toString()} m"),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Temperature",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("${post.temperature.toString()} °C"),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Text(post.description),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostEditView extends StatelessWidget {
+  final Post post;
+  final User user;
+  const PostEditView({required this.post, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Post"),
+      ),
+      body: Text("Edit Post: ${post.title}"),
+    );
+  }
+}
--- lib/views/post_tile.dart
+++ lib/views/post_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:snowpack/models/post.dart';
 import 'package:snowpack/models/user.dart';
+import 'package:snowpack/views/post_info_screen.dart';
 
 class PostTile extends StatelessWidget {
   final Post post;
@@ -120,113 +121,6 @@ class PostTile extends StatelessWidget {
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class PostInfoScreen extends StatelessWidget {
-  final Post post;
-  final User user;
-
-  const PostInfoScreen({super.key, required this.post, required this.user});
-
-  String getFormattedDate() {
-    return "${post.dateTime.day}/${post.dateTime.month}/${post.dateTime.year} at ${post.dateTime.hour}:${post.dateTime.minute}";
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(post.title)),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(
-                  CupertinoIcons.person_alt_circle,
-                  size: 32,
-                ),
-                const SizedBox(width: 8),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      user.username,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Text(
-                      getFormattedDate(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 10,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text(
-              post.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Aspect",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(post.aspect.toString().split('.').last),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Elevation",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text("${post.elevation.toString()} m"),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Temperature",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text("${post.temperature.toString()} °C"),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(post.description),
-          ],
         ),
       ),
     );
